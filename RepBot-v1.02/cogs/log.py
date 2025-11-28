99d57e97e1e032310bea7a6a9b0433c4db267839
TheGreatLordd <auburn300jtn@proton.me>
2025-11-28 13:40:55 -0800

    Refactor LoggerCog setup and import statements

--- RepBot-v1.02/cogs/log.py
+++ RepBot-v1.02/cogs/log.py
@@ -11,10 +11,10 @@ class LoggerCog(commands.Cog):
         self.logger = logging.getLogger("repbot")
         
     async def send_log(self, message: str, *, embed: discord.Embed = None):
-        '''Sends a message or embed to the requested log channel'''
         channel = self.bot.get_channel(config.LOG_ID)
         await channel.send(content=message, embed=embed) if channel else self.logger.info(f'Channel is not a valid ChannelID or is None. Type: {type(channel)}')
     
 
 async def setup(bot):
+
     await bot.add_cog(LoggerCog(bot))
